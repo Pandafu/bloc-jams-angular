68b55230c1b9fc3d97194e90d22364b16fa880d3
Pandafu <developermode@Risss-MacBook-Pro.local>
2017-01-02 11:46:14 -0600

    completed assignment 8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -106,6 +106,20 @@
             } /* 13 */
         }; /* 11 */
         
+        SongPlayer.next = function(){
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            if(currentSongIndex < 1){
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            }else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
         return SongPlayer;
     };
  
